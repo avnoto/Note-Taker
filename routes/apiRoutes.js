3fdfa323e75f5a20168880d1cbbcc120860f721c
avnoto <alexandravnoto@gmail.com>
2020-06-26 16:54:19 -0700

    changed module exports function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,22 @@
 const connection = require('../db/connection');
+const app = require('express').Router();
 
-module.exports = function (app) {
-  app.get('/api/notes', function (req, res) {
-    connection.query('SELECT * FROM notes', (err, response) => {
-      return res.json(response);
-    });
+app.get('/api/notes', function (req, res) {
+  connection.query('SELECT * FROM notes', (err, response) => {
+    return res.json(response);
   });
-  app.post('/api/notes', function (req, res) {
-    connection.query('INSERT INTO notes SET ?', req.body, (err, response) => {
-      return res.json(response);
-    });
+});
+app.post('/api/notes', function (req, res) {
+  connection.query('INSERT INTO notes SET ?', req.body, (err, response) => {
+    return res.json(response);
   });
-  app.delete('/api/notes/:id', function (req, res) {
-    const id = req.params.id;
+});
+app.delete('/api/notes/:id', function (req, res) {
+  const id = req.params.id;
 
-    connection.query('DELETE FROM notes WHERE ?', { id }, (err, response) => {
-      return res.json(response);
-    });
+  connection.query('DELETE FROM notes WHERE ?', { id }, (err, response) => {
+    return res.json(response);
   });
-};
+});
+
+module.exports = app;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const app = require('express').Router();
 
-module.exports = function (app) {
-  app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-};
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;
