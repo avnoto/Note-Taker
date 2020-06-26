4da28da04964624efbc65c958b789ae16efe00c9
avnoto <alexandravnoto@gmail.com>
2020-06-26 16:30:41 -0700

    removed local connection if/else statements in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,16 @@
 const mysql = require('mysql');
 
-let connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'notes_db',
-  });
-}
+// if (process.env.JAWSDB_URL) {
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 connection.connect();
 module.exports = connection;
+// } else {
+//   const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'root',
+//     database: 'notes_db',
+//   });
+//   connection.connect();
+// }
